6d972c09b4c8d0d38e68914a7515db4ff15e3c03
avalos010 <luizavalos40@gmail.com>
2020-01-21 12:39:30 -0600

    hid container overflow and removed extra margin bottom added by js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ const element = document.querySelectorAll('.details__tile')
 if (element) {
     element.forEach(function(el) {
         el.addEventListener('click', function() {
-            console.log(this.style)
             if (this.querySelector('.details__info').style.height == '200px') {
                 this.querySelector('.details__info').style.height = '0px';
                 this.querySelector('.details__info').style.opacity = '0';
-                this.style.marginBottom = '40px';
 
             } else {
                 this.querySelector('.details__info').style.height = '200px';
